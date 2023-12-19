9a6ba997b5983306ba101140387baa7ede267a85
Pedro Castro <128812446+castp3@users.noreply.github.com>
2023-12-19 15:40:00 -0300

    Add files via upload

---
+++ ConversorTemperatura/src/main/java/com/mycompany/conversortemperatura/ConversorTemperatura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.conversortemperatura;
+
+/**
+ *
+ * @author pedro_p5b3c1j
+ */
+public class ConversorTemperatura {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
